14e90e4f54ed0606da3f26df0dadcdf3347fb5b3
joshua sorkin <joshua.d.sorkin@gmail.com>
2018-03-05 18:27:43 -0800

    retry with functions in resolve/reject

--- src/ivr/router.js
+++ src/ivr/router.js
@@ -26,11 +26,11 @@ router.post('/welcome_promise',(req,res) => {
 	const fromNum=req.body.From;
 	const sid=req.body.CallSid;
 	console.log("/welcome_promise: sid "+sid);
-	db.getUser(fromNum).then(
+	db.getUser(fromNum).then(value=>{
 		console.log("/welcome_promise: then");
-	).catch(
+	}).catch(error=>{
 		console.log("/welcome_promise: catch");
-	);
+	});
 });
 
 
