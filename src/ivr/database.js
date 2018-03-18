4c7298090f73b6f5a56877048b49f479a2013f7a
joshua sorkin <joshua.d.sorkin@gmail.com>
2018-03-18 13:32:20 -0700

    fix syntax

--- src/ivr/database.js
+++ src/ivr/database.js
@@ -29,7 +29,7 @@ module.exports = {
 				if (err){
 					console.log("getUser_promise error: "+err.toString());
 					reject(err);				}
-				elseif (res.rows.length==0){
+				else if (res.rows.length==0){
 					console.log("getUser_promise: null, need to add user");
 					resolve(res);
 				}
