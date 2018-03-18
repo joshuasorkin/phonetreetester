15ed70486f2d187d22ca78c973a560d7b1deeaed
joshua sorkin <joshua.d.sorkin@gmail.com>
2018-03-18 13:45:58 -0700

    logging value from second then

--- src/ivr/router.js
+++ src/ivr/router.js
@@ -52,6 +52,8 @@ router.post('/welcome_promise',(req,res) => {
 		console.log("/welcome_promise: error first then");
 		console.log("/welcome_promise: "+error.toString());	
 	}).then(value=>{
+		console.log("/welcome_promise: second then");
+		console.log("/welcome_promise: value "+value.rows[0]);
 		if (id==null){
 			id=value.rows[0].id;
 		}
